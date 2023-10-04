9d0812c9c886d66c5d5ef8ea102be2824558495a
Rivka Sharfman <rivkasa@gmail.com>
2023-10-04 13:45:44 +0300

    horuku app setup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 
@@ -88,6 +89,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen([port], () => {
+    console.log('Server is up on port ' + port + '.')
 })
